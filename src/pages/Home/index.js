eb342ae356fe29b7467835573987a5746d8fa54d
Elias <izhengchengjin@outlook.com>
2022-07-04 23:07:02 +0800

    Edit Home Page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,59 @@
 import Bar from "@/components/Bar";
+import { DatePicker, Input, Space } from "antd";
+import "./index.scss";
+import {
+  ProCard,
+  ProForm,
+  ProFormDateRangePicker,
+  ProFormText,
+} from "@ant-design/pro-components";
+import "moment/locale/zh-cn";
+import locale from "antd/es/date-picker/locale/zh_CN";
+import { useEffect, useState } from "react";
+import { http } from "@/utils";
+
+const { RangePicker } = DatePicker;
 
 function Home() {
+  const [params, setParams] = useState({});
+
+  async function submitForm(values) {
+    setParams(values);
+    const res = await http.get("/amazon/analyze-data/list", { ...params });
+    console.log(res);
+  }
+
   return (
-    <div>
-      <Bar
-        title={"主流框架使用满意度"}
-        xData={["react", "vue", "angular", "jquery"]}
-        yData={[88, 79, 67, 50]}
-        style={{ width: "500px", height: "400px" }}
-      />
-      <Bar
-        title={"主流框架使用满意度2"}
-        xData={["react", "vue", "angular", "jquery"]}
-        yData={[81, 69, 75, 40]}
-        style={{ width: "500px", height: "200px" }}
-      />
+    <div style={{ height: "100%" }}>
+      <ProCard
+        title="数据分析"
+        extra={
+          <ProForm
+            layout={"horizontal"}
+            onFinish={submitForm}
+            style={{ height: "100%" }}
+          >
+            <Space>
+              <ProFormDateRangePicker
+                transform={(values) => {
+                  return {
+                    startTime: values ? values[0] : undefined,
+                    endTime: values ? values[1] : undefined,
+                  };
+                }}
+                name={"dateRange"}
+                locale={locale}
+                required={true}
+              />
+              <ProFormText
+                name={"asin"}
+                style={{ width: "100px", marginRight: "10px" }}
+                required={true}
+              />
+            </Space>
+          </ProForm>
+        }
+      ></ProCard>
     </div>
   );
 }
